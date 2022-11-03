7c45f7d39474511ce536e55a2aa326c4a22f6124
boortel <33236294+boortel@users.noreply.github.com>
2022-11-03 15:33:01 +0200

    Replacement of data generators

--- ModelEvaluation.py
+++ ModelEvaluation.py
@@ -9,24 +9,24 @@ This class is used for the evaluation of the trained model
 """
 
 import os
-import sys
 import logging
 import traceback
+
+import cv2 as cv
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
 from scipy.io import savemat
 
 from keras.models import Model, load_model
-from keras.preprocessing.image import ImageDataGenerator
 
 class ModelEvaluation():
 
     ## Set the constants and paths
-    def __init__(self, modelBasePath, datasetPathEv, labelsPath, modelSel, labelInfo, imageDim, batchSizeEv, numEpochEv):
+    def __init__(self, modelBasePath, datasetPathEv, modelSel, labelInfo, imageDim, batchSizeEv, numEpochEv):
 
         # Set paths
-        self.labelsPath = labelsPath
         self.modelPath = os.path.join(modelBasePath, modelSel + labelInfo)
         self.datasetPath = datasetPathEv
 
@@ -42,43 +42,23 @@ class ModelEvaluation():
 
         # Set image generator and get labels to save in npz
         self.setGenerators()
-        self.getLabels()
 
         # Encode, decode and visualise the training data
         _ = self.dataEncodeDecode('Train')
         _ = self.dataEncodeDecode('Test')
 
+
     ## Normalize the input data
     def NormalizeData(self, data):
 
         return (data - np.min(data)) / (np.max(data) - np.min(data))
-    
-
-    ## Read all labels from the npz file
-    def getLabels(self):
-        try:
-            # Load npz data
-            data = np.load(self.labelsPath)
-
-            try:
-                self.train_label = data['train']
-                self.valid_label = data['valid']
-                self.test_label = data['test']
-            except:
-                print('Loading data should be numpy array and has "images" and "labels" keys.')
-                sys.exit(1)
-        except:
-            logging.error(': Loading the labels failed...')
-            traceback.print_exc()
-
-        else:
-            logging.info(': Loading the labels was succesful...')
 
 
     ## Set the image generators and its parameters
     def setGenerators(self):
         
         try:
+            # Set the image size
             tSize = (self.imageDim[0], self.imageDim[1])
 
             if self.imageDim[2] == 3:
@@ -86,56 +66,58 @@ class ModelEvaluation():
             else:
                 cMode = 'gray'
 
-            train_datagen = ImageDataGenerator(
-                rescale = 1./255)
-                # samplewise_center = True,
-                # samplewise_std_normalization = True)
-            
-            test_datagen = ImageDataGenerator(
-                rescale = 1./255)
-                # samplewise_center = True,
-                # samplewise_std_normalization = True)
-            
-            # Data generator for train data
-            self.train_generator = train_datagen.flow_from_directory(
-                (self.datasetPath + 'train'), 
-                target_size = tSize, 
-                batch_size = self.batchSize,
+            # Define normalization layer
+            normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+            # Train DS
+            dsTrain = tf.keras.utils.image_dataset_from_directory(
+                (self.datasetPath + 'train'),
+                image_size = tSize,
                 color_mode = cMode,
-                class_mode = 'input',
+                batch_size = self.batchSize,
+                crop_to_aspect_ratio = True,
+                label_mode = None,
                 shuffle = False)
 
-            self.train_generatorL = train_datagen.flow_from_directory(
-                (self.datasetPath + 'train'), 
-                target_size = tSize, 
-                batch_size = self.batchSize,
+            self.dsTrain = dsTrain.map(lambda x: (normalization_layer(x)))
+
+            self.dsTrainL = tf.keras.utils.image_dataset_from_directory(
+                (self.datasetPath + 'train'),
+                image_size = tSize,
                 color_mode = cMode,
-                class_mode = 'binary',
-                shuffle = False)
-            
-            # Data generator for test data
-            self.test_generator = test_datagen.flow_from_directory(
-                (self.datasetPath + 'test'), 
-                target_size = tSize, 
                 batch_size = self.batchSize,
-                color_mode = cMode,
-                class_mode = 'input',
+                crop_to_aspect_ratio = True,
+                label_mode = 'int',
                 shuffle = False)
 
-            self.test_generatorL = test_datagen.flow_from_directory(
-                (self.datasetPath + 'test'), 
-                target_size = tSize, 
-                batch_size = self.batchSize,
+            # Test DS
+            dsTest = tf.keras.utils.image_dataset_from_directory(
+                (self.datasetPath + 'test'),
+                image_size = tSize,
                 color_mode = cMode,
-                class_mode = 'binary',
+                batch_size = self.batchSize,
+                crop_to_aspect_ratio = True,
+                label_mode = None,
+                shuffle = False)
+
+            self.dsTest = dsTest.map(lambda x: (normalization_layer(x)))
+
+            self.dsTestL = tf.keras.utils.image_dataset_from_directory(
+                (self.datasetPath + 'test'),
+                image_size = tSize,
+                color_mode = cMode,
+                batch_size = self.batchSize,
+                crop_to_aspect_ratio = True,
+                label_mode = 'int',
                 shuffle = False)
 
         except:
-            logging.error(': Setting up the data generators failed...')
+            logging.error(': Loading of the datasets failed...')
             traceback.print_exc()
+            return
 
         else:
-            logging.info(': Setting up the data generators was succesful...')
+            logging.info(': Loading of the datasets was succesful...')
 
 
     ## Get the encoded and decoded data from selected model and dataset
@@ -146,13 +128,11 @@ class ModelEvaluation():
         try:
             # Set the train or test data
             if actStr == 'Train':
-                data_generator = self.train_generator
-                data_generatorL = self.train_generatorL
-                #labels = self.train_label
+                dataset = self.dsTrain
+                datasetL = self.dsTrainL
             else:
-                data_generator = self.test_generator
-                data_generatorL = self.test_generatorL
-                #labels = self.test_label
+                dataset = self.dsTest
+                datasetL = self.dsTestL
 
             outputPath = os.path.join(self.modelPath, 'modelData', 'Eval_' + actStr)
 
@@ -161,15 +141,14 @@ class ModelEvaluation():
             
             # Get the encoded data
             encoder = Model(inputs = autoencoder.input, outputs = autoencoder.get_layer('enc').output)
-            enc_out = self.NormalizeData(encoder.predict(data_generator))
+            enc_out = self.NormalizeData(encoder.predict(dataset))
 
             # Get the decoded data
-            dec_out = self.NormalizeData(autoencoder.predict(data_generator))
+            dec_out = self.NormalizeData(autoencoder.predict(dataset))
 
             # Get the original data to be saved in npz
-            data_generator.reset()
-            orig_data = np.concatenate([data_generator.next()[0] for i in range(data_generator.__len__())])
-            labels = np.concatenate([data_generatorL.next()[1] for i in range(data_generatorL.__len__())])
+            orig_data = np.concatenate([img for img in dataset], axis=0)
+            labels = np.concatenate([labels for _, labels in datasetL], axis=0)
 
             # Get the difference images (org - dec)
             diff_data = np.subtract(orig_data, dec_out)
@@ -181,7 +160,7 @@ class ModelEvaluation():
             processedData = {'Org': orig_data, 'Enc': enc_out, 'Dec': dec_out, 'Dif': diff_data, 'Lab': labels}
             
             # Save the obtained data to NPZ and MAT
-            np.savez(outputPath, orgData = orig_data, encData = enc_out, decData = dec_out, difData = diff_data, labels = labels)
+            np.savez_compressed(outputPath, orgData = orig_data, encData = enc_out, decData = dec_out, difData = diff_data, labels = labels)
             #savemat(outputPath + '.mat', processedData, do_compression = True)
 
             # Visualise the obtained data
@@ -190,6 +169,7 @@ class ModelEvaluation():
         except:
             logging.error(': Data encode and decode for the model ' + self.modelName + ' failed...')
             traceback.print_exc()
+            return
 
         else:
             logging.info(': Data encode and decode for the model ' + self.modelName + ' was succesful...')
@@ -217,48 +197,48 @@ class ModelEvaluation():
             fig, axarr = plt.subplots(4,4)
             fig.set_size_inches(16, 16)
 
-            fig.suptitle('Original, encoded and decoded images of the ' + self.modelName + ' autoencoder model' + label)
+            fig.suptitle('Original, encoded and decoded images of the ' + self.modelName + ' autoencoder model' + label, fontsize=14, y=1.08)
 
             # Cookie: 27, 35, 205
             
             axarr[0,0].set_title("Original")
-            axarr[0,0].imshow(orig_data[0])
+            axarr[0,0].imshow(cv.normalize(orig_data[0], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[0,0].axis('off')
-            axarr[1,0].imshow(orig_data[10])
+            axarr[1,0].imshow(cv.normalize(orig_data[10], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[1,0].axis('off')
-            axarr[2,0].imshow(orig_data[25])
+            axarr[2,0].imshow(cv.normalize(orig_data[25], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[2,0].axis('off')
-            axarr[3,0].imshow(orig_data[35])
+            axarr[3,0].imshow(cv.normalize(orig_data[35], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[3,0].axis('off')
             
             axarr[0,1].set_title("Encoded")
-            axarr[0,1].imshow(enc_out[0])
+            axarr[0,1].imshow(cv.normalize(enc_out[0], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[0,1].axis('off')
-            axarr[1,1].imshow(enc_out[10])
+            axarr[1,1].imshow(cv.normalize(enc_out[10], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[1,1].axis('off')
-            axarr[2,1].imshow(enc_out[25])
+            axarr[2,1].imshow(cv.normalize(enc_out[25], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[2,1].axis('off')
-            axarr[3,1].imshow(enc_out[35])
+            axarr[3,1].imshow(cv.normalize(enc_out[35], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[3,1].axis('off')
 
             axarr[0,2].set_title("Decoded")
-            axarr[0,2].imshow(dec_out[0])
+            axarr[0,2].imshow(cv.normalize(dec_out[0], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[0,2].axis('off')
-            axarr[1,2].imshow(dec_out[10])
+            axarr[1,2].imshow(cv.normalize(dec_out[10], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[1,2].axis('off')
-            axarr[2,2].imshow(dec_out[25])
+            axarr[2,2].imshow(cv.normalize(dec_out[25], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[2,2].axis('off')
-            axarr[3,2].imshow(dec_out[35])
+            axarr[3,2].imshow(cv.normalize(dec_out[35], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[3,2].axis('off')
 
             axarr[0,3].set_title("Diff image")
-            axarr[0,3].imshow(diff_data[0])
+            axarr[0,3].imshow(cv.normalize(diff_data[0], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[0,3].axis('off')
-            axarr[1,3].imshow(diff_data[10])
+            axarr[1,3].imshow(cv.normalize(diff_data[10], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[1,3].axis('off')
-            axarr[2,3].imshow(diff_data[25])
+            axarr[2,3].imshow(cv.normalize(diff_data[25], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[2,3].axis('off')
-            axarr[3,3].imshow(diff_data[35])
+            axarr[3,3].imshow(cv.normalize(diff_data[35], None, 0, 255, cv.NORM_MINMAX, cv.CV_8U))
             axarr[3,3].axis('off')
 
             # Save the illustration figure
--- ModelTrain.py
+++ ModelTrain.py
@@ -11,12 +11,15 @@ This class is used for training the selected model
 import os
 import logging
 import traceback
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
-from ModelSaved import ModelSaved
-
 from keras import callbacks
-from keras.preprocessing.image import ImageDataGenerator
+from functools import partial
+from ModelSaved import ModelSaved
+from albumentations import Compose, RandomBrightness, JpegCompression, HueSaturationValue, RandomContrast, HorizontalFlip, Rotate
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 class ModelTrain():
 
@@ -45,6 +48,16 @@ class ModelTrain():
         modelObj = ModelSaved(self.modelName, self.imageDim, intermediateDim = 64)
 
         self.model = modelObj.model
+
+        # Set the augmentations
+        self.transforms = Compose([
+            Rotate(limit=40),
+            RandomBrightness(limit=0.1),
+            JpegCompression(quality_lower=85, quality_upper=100, p=0.5),
+            HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20, p=0.5),
+            RandomContrast(limit=0.2, p=0.5),
+            HorizontalFlip(),
+        ])
         
         # Set generators and callback
         self.setGenerators()
@@ -67,42 +80,36 @@ class ModelTrain():
             else:
                 cMode = 'gray'
 
-            # Path constants
-            train_dir = self.datasetPathTr + 'train'
-            validation_dir = self.datasetPathTr + 'valid'
-            
-            # Load the normalized images
-            train_datagen = ImageDataGenerator(
-                rescale = 1./255,
-                # samplewise_center = True,
-                # samplewise_std_normalization = True,
-                rotation_range = 360,
-                width_shift_range = 0.05,
-                height_shift_range = 0.05,
-                horizontal_flip = True)
-            
-            validation_datagen = ImageDataGenerator(
-                rescale = 1./255)
-                # samplewise_center = True,
-                # samplewise_std_normalization = True)
-                
-            # Data generator for training data
-            self.train_generator = train_datagen.flow_from_directory(
-                train_dir,
-                target_size = tSize, 
-                batch_size = self.batchSize,
+            # Define normalization layer
+            normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+            # Train DS
+            dsTrain = tf.keras.utils.image_dataset_from_directory(
+                (self.datasetPathTr + 'train'),
+                image_size = tSize,
                 color_mode = cMode,
-                class_mode = 'input',
-                shuffle = True)
-            
-            # Data generator for validation data
-            self.validation_generator = validation_datagen.flow_from_directory(
-                validation_dir, 
-                target_size = tSize, 
                 batch_size = self.batchSize,
-                color_mode = cMode,
-                class_mode = 'input', 
+                crop_to_aspect_ratio = True,
+                label_mode = None,
                 shuffle = False)
+
+            # Augment the dataset (with normalization) and reset its shapes
+            dsTrain = dsTrain.map(partial(self.process_data), num_parallel_calls = AUTOTUNE).prefetch(AUTOTUNE)
+            self.dsTrain = dsTrain.map(self.set_shapes, num_parallel_calls = AUTOTUNE).batch(32).prefetch(AUTOTUNE)
+
+            # Validation DS
+            dsValid = tf.keras.utils.image_dataset_from_directory(
+                (self.datasetPathTr + 'valid'),
+                image_size = tSize,
+                color_mode = cMode,
+                batch_size = self.batchSize,
+                crop_to_aspect_ratio = True,
+                label_mode = None,
+                shuffle = False)
+
+            # Normalize the dataset
+            self.dsValid = dsValid.map(lambda x: (normalization_layer(x)))
+            
         except:
             logging.error(': Data generators initialization of the ' + self.modelName + ' model failed...')
             traceback.print_exc()
@@ -126,7 +133,7 @@ class ModelTrain():
             # Configure the early stopping callback
             self.esCallBack = callbacks.EarlyStopping(
                 monitor = 'loss', 
-                patience = 3)
+                patience = 6)
 
         except:
             logging.error(': Callback initialization of the ' + self.modelName + ' model failed...')
@@ -136,18 +143,47 @@ class ModelTrain():
             logging.info(': Callback of the ' + self.modelName + ' model initialized...')
 
 
+    ## Help function for albumentation - aug_fn
+    def aug_fn(self, image):
+        data = {"image":image}
+        aug_data = self.transforms(**data)
+        aug_img = aug_data["image"]
+        aug_img = tf.cast(aug_img/255.0, tf.float32)
+        aug_img = tf.image.resize(aug_img, size=[self.imageDim[0], self.imageDim[1]])
+
+        return aug_img
+
+
+    ## Help function for albumentation - process data
+    def process_data(self, image, label):
+        aug_img = tf.numpy_function(func = self.aug_fn, inp = image, Tout = tf.float32)
+
+        return aug_img, label
+
+
+    ## Help function for albumentation - set shapes
+    def set_shapes(self, img, label):
+        img.set_shape(self.imageDim)
+        label.set_shape([])
+
+        return img, label
+
+
     ## Compile and train the model
     def modelTrain(self):
 
         try:
+            # Define the class weights
+            class_weight = {0: 1., 
+                            1: 50.}
+
             # Train the model
             self.trainHistory = self.model.fit(
-                self.train_generator, 
-                steps_per_epoch = self.train_generator.samples/self.train_generator.batch_size, 
+                self.dsTrain,
                 epochs = self.numEpoch,   
-                validation_data = self.validation_generator, 
-                validation_steps = self.validation_generator.samples/self.validation_generator.batch_size, 
+                validation_data = self.dsValid, 
                 verbose = 1,
+                class_weight = class_weight,
                 callbacks = [self.esCallBack])
                 # callbacks = [tbCallBack, esCallBack])
             
