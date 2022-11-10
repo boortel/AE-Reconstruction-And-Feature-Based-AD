d7b4078401bee8bfe9c2d871373ba321deaa8886
boortel <33236294+boortel@users.noreply.github.com>
2022-11-10 13:36:18 +0200

    Variational AEs
    Possible problems with datasets loading for not VAE models

---
+++ ModelHelperVAE.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  9 17:23:05 2021
+
+@author: Simon Bilik
+
+This is a helper class for the VAE and VQ-VAE models.
+
+Please refer to following examples:
+
+    - https://keras.io/examples/generative/vae/
+    - https://keras.io/examples/generative/vq_vae/
+
+"""
+
+import keras
+import tensorflow as tf
+
+from keras.models import Model
+from keras.layers import Layer
+
+## Class to define and train VAEs models
+class VAE(Model):
+    
+    def __init__(self, inputs, outputs, encoder, decoder, modelName, **kwargs):
+        super(VAE, self).__init__(inputs, outputs, modelName, **kwargs)
+        
+        self.encoder = encoder
+        self.decoder = decoder
+        
+        self.total_loss_tracker = keras.metrics.Mean(name = "total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(name = "reconstruction_loss")
+        self.kl_loss_tracker = keras.metrics.Mean(name = "kl_loss")
+
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+
+    def train_step(self, data):
+        with tf.GradientTape() as tape:
+            # Outputs from the VAE
+            z_mean, z_log_var, z = self.encoder(data)
+            #reconstruction = self.vae(data)
+            reconstruction = self.decoder(z)
+            
+            # Calculate the losses.
+            reconstruction_loss = tf.reduce_mean(
+                tf.reduce_sum(
+                    keras.losses.binary_crossentropy(data, reconstruction), axis=(1, 2)
+                )
+            )
+            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+            total_loss = reconstruction_loss + kl_loss
+        
+        # Backpropagation
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        
+        # Loss tracking
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        
+        # Log results
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+
+
+## Sampling layer for VAEs
+class Sampling(Layer):
+    """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
+
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+    
+    
+    
+## Help class for VQ-VAEs
+class VectorQuantizer(Layer):
+    def __init__(self, num_embeddings, embedding_dim, beta=0.25, **kwargs):
+        super().__init__(**kwargs)
+        self.embedding_dim = embedding_dim
+        self.num_embeddings = num_embeddings
+
+        # The `beta` parameter is best kept between [0.25, 2] as per the paper
+        self.beta = beta
+
+        # Initialize the embeddings which we will quantize
+        w_init = tf.random_uniform_initializer()
+        self.embeddings = tf.Variable(
+            initial_value=w_init(
+                shape=(self.embedding_dim, self.num_embeddings), dtype="float32"
+            ),
+            trainable=True,
+            name="embeddings_vqvae",
+        )
+
+    def call(self, x):
+        # Calculate the input shape of the inputs and
+        # then flatten the inputs keeping `embedding_dim` intact
+        input_shape = tf.shape(x)
+        flattened = tf.reshape(x, [-1, self.embedding_dim])
+
+        # Quantization.
+        encoding_indices = self.get_code_indices(flattened)
+        encodings = tf.one_hot(encoding_indices, self.num_embeddings)
+        quantized = tf.matmul(encodings, self.embeddings, transpose_b=True)
+
+        # Reshape the quantized values back to the original input shape
+        quantized = tf.reshape(quantized, input_shape)
+
+        # Calculate vector quantization loss and add that to the layer. You can learn more
+        # about adding losses to different layers here:
+        # https://keras.io/guides/making_new_layers_and_models_via_subclassing/. Check
+        # the original paper to get a handle on the formulation of the loss function
+        commitment_loss = tf.reduce_mean((tf.stop_gradient(quantized) - x) ** 2)
+        codebook_loss = tf.reduce_mean((quantized - tf.stop_gradient(x)) ** 2)
+        self.add_loss(self.beta * commitment_loss + codebook_loss)
+
+        # Straight-through estimator.
+        quantized = x + tf.stop_gradient(quantized - x)
+        return quantized
+
+    def get_code_indices(self, flattened_inputs):
+        # Calculate L2-normalized distance between the inputs and the codes
+        similarity = tf.matmul(flattened_inputs, self.embeddings)
+        distances = (
+            tf.reduce_sum(flattened_inputs ** 2, axis=1, keepdims = True)
+            + tf.reduce_sum(self.embeddings ** 2, axis=0)
+            - 2 * similarity
+        )
+
+        # Derive the indices for minimum distances
+        encoding_indices = tf.argmin(distances, axis=1)
+        return encoding_indices
+
+
+## Class to define and train the VQ-VAE models
+class VQVAETrainer(Model):
+    def __init__(self, inputs, outputs, encoder, decoder, modelName, train_variance, latent_dim=32, num_embeddings=128, **kwargs):
+        super(VQVAETrainer, self).__init__(inputs, outputs, modelName, **kwargs)
+        self.train_variance = train_variance
+        self.latent_dim = latent_dim
+        self.num_embeddings = num_embeddings
+
+        self.vqvae = self.get_vqvae(encoder, decoder, self.latent_dim, self.num_embeddings)
+
+        self.total_loss_tracker = keras.metrics.Mean(name = "total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(name = "reconstruction_loss")
+        self.vq_loss_tracker = keras.metrics.Mean(name = "vq_loss")
+        
+    def get_vqvae(self, encoder, decoder, latent_dim=16, num_embeddings=64):
+        vq_layer = VectorQuantizer(num_embeddings, latent_dim, name = "vector_quantizer")
+        
+        inputs = keras.Input(shape=(28, 28, 1))
+        encoder_outputs = encoder(inputs)
+        
+        quantized_latents = vq_layer(encoder_outputs)
+        reconstructions = decoder(quantized_latents)
+        
+        return keras.Model(inputs, reconstructions, name = "vq_vae")
+
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.vq_loss_tracker,
+        ]
+
+    def train_step(self, x):
+        with tf.GradientTape() as tape:
+            # Outputs from the VQ-VAE
+            reconstructions = self.vqvae(x)
+
+            # Calculate the losses
+            reconstruction_loss = (
+                tf.reduce_mean((x - reconstructions) ** 2) / self.train_variance
+            )
+            total_loss = reconstruction_loss + sum(self.vqvae.losses)
+
+        # Backpropagation
+        grads = tape.gradient(total_loss, self.vqvae.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.vqvae.trainable_variables))
+
+        # Loss tracking
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.vq_loss_tracker.update_state(sum(self.vqvae.losses))
+
+        # Log results
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "vqvae_loss": self.vq_loss_tracker.result(),
+        }
